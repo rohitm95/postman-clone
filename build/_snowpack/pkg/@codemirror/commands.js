f8b4c938edd46a0c41c1f54afd7b5d53e592200f
Rohit Mamidwar <rohitm95@gmail.com>
2021-07-27 14:24:25 +0530

    removed build config files

--- build/_snowpack/pkg/@codemirror/commands.js
+++
@@ -1,3 +0,0 @@
-export { a as defaultTabBinding } from '../common/index-de6d4943.js';
-import '../common/index-ba43352c.js';
-import '../common/index-c8b4f5d5.js';
--- build/_snowpack/pkg/@codemirror/view.js
+++
@@ -1,1 +0,0 @@
-export { E as EditorView, n as keymap } from '../common/index-ba43352c.js';
